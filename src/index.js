75e4644bdc0f3a4c233bc3b2b4a86931b6336323
Ron B <anydev1103@gmail.com>
2021-09-20 00:13:07 +0900

    Update index.js
    Remove unused comments

--- src/index.js
+++ src/index.js
@@ -10,16 +10,6 @@ import { Provider } from 'react-redux';
 
 const store = createStore(reducers);
 
-// console.log(store.getState());
-// const unsubscribe = store.subscribe(() => console.log(store.getState()));
-
-// store.dispatch(actions.increment());
-// store.dispatch(actions.increment());
-// store.dispatch(actions.decrement());
-// store.dispatch(actions.setColor([210, 210, 210]));
-
-// unsubscribe();
-
 ReactDOM.render(
     <Provider store={store}>
         <App/>
